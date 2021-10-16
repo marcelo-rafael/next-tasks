317b4d18c3aa45e9225dd0cd8dced720431ef4e9
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-16 16:42:35 -0300

    Criando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex flex-col bg-purple-600 
+      justify-center items-center h-screen
+      text-white
+    `}>
+     <span>1</span>
+     <span>2</span>
+     <span>3</span>
+    </div>
+  )
+}
