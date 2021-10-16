3d650d15571f0e7c5aecd33bf3d2c158af856a04
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-16 16:42:50 -0300

    Classe tarefa

---
+++ src/model/Task.ts
@@ -0,0 +1,43 @@
+export default class Task {
+  #id: number
+  #description: string
+  #completed: boolean
+
+  constructor(id: number, description: string, completed = false) {
+    this.#id = id
+    this.#description = description
+    this.#completed = completed
+  }
+
+  static createActive(id: number, description: string) {
+    return new Task(id, description)
+  }
+
+  static createCompleted(id: number, description: string) {
+    return new Task(id, description, true)
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get description() {
+    return this.#description
+  }
+
+  get completed() {
+    return this.#completed
+  }
+
+  toggleStatus() {
+    return this.completed ? this.activate() : this.conclude()
+  }
+
+  conclude() {
+    return Task.createCompleted(this.id, this.#description)
+  }
+
+  activate() {
+    return Task.createActive(this.id, this.description)
+  }
+}
