c04adc58cd17426197e6ba905e72e61c02a59750
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-17 18:13:54 -0300

    Componente formulário

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Task from '../../model/Task'
+
+interface FormProps {
+  newTaskCreated: (task: Task) => void
+}
+
+export default function Form(props: FormProps) {
+  const [description, setDescription] = useState('')
+
+  function createNewTask() {
+    if(description?.trim().length > 0) {
+      const newTask =Task.createActive(Math.random(), description)
+      props.newTaskCreated(newTask)
+      setDescription('')
+    }
+  }
+
+  return (
+    <div className="flex flex-1 justify-center">
+      <input 
+        type="text" 
+        value={description}
+        placeholder="Informe sua próxima tarefa"
+        onChange={event => setDescription(event.target.value)}
+        onKeyDown={event => event.key === 'Enter' ? createNewTask() : false}
+        className={`
+          w-1/2 py-2 px-3 rounded-lg border-2 text-2xl
+          border-purple-300 focus:outline-none
+          focus:ring-2 focus:ring-purple-600
+        `}
+      />
+      <button
+        onClick={createNewTask}
+        className={`
+          ml-3 px-5 py-4 focus:outline-none rounded-lg
+          bg-purple-600 text-white text-xl
+        `}
+      >
+        +
+      </button>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
+
+import Form from '../components/form/Form'
 import List from '../components/list/List'
 import Content from '../components/template/Content'
 import Header from '../components/template/Header'
 import initialTasks from '../data/mocks'
+import Task from '../model/Task'
 
 export default function Home() {
   const [tasks, setTasks] = useState(initialTasks)
+
+  function newTaskCreated(newTask: Task) {
+    setTasks(tasks.addTask(newTask))
+  }
 
   return (
     <div className={`
       flex flex-col bg-gray-300 h-screen
     `}>
       <Header>
+        <Form newTaskCreated={newTaskCreated} />
       </Header>
 
       <Content>
