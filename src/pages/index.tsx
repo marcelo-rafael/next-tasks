66d93e1f0fa9ec7d2b4ef676c8e97a2ddbec820f
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-17 02:22:27 -0300

    Componente seleção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,14 @@
-import Task from '../model/Task'
-import initialTasks from '../data/mocks'
+import Selection from '../components/lista/Selection';
 
 export default function Home() {
-  let tasks = initialTasks
-  tasks = tasks.filterActives()
-  tasks = tasks.filterCompleted()
-  // tasks = tasks.RemoveFilter()
-  tasks = tasks.deleteCompleted()
-
-  tasks = tasks.addTask(Task.createCompleted(6, 'Lavar os pratos!!!'))
-  tasks = tasks.addTask(Task.createActive(7, 'Cuida das crianças!!!'))
-  tasks = tasks.deleteCompleted()
-
-  tasks = tasks.modifyTask(tasks.items[1].toggleStatus())
-  tasks = tasks.modifyTask(tasks.items[2].toggleStatus())
-
-  function renderTasks() {
-    return tasks.items.map(task => {
-      return (
-        <div key={task.id}>
-          <span>{task.id} | </span>
-          <span>{task.description} | </span>
-          <span>{task.completed ? 'Concluída' : 'Ativa'}</span>
-        </div>
-      )
-    })
-  }
-
   return (
     <div className={`
       flex flex-col bg-purple-600 
       justify-center items-center h-screen
       text-white
     `}>
-    {renderTasks()}
+      <Selection value={true}  />
+      <Selection value={false}  />
     </div>
   )
 }
